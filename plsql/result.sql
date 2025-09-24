aa4d3cce5170a83dd28dad19edd442a2e0bf93d3
Siddeshborade <siddeshborade@gmail.com>
2025-09-24 19:32:33 +0530

    result.sql

--- plsql/result.sql
+++ plsql/result.sql
@@ -1,14 +1,13 @@
-declare
-r int;
-total int;
-nm varchar2(20);
-gr varchar2(20);
-begin
-r:=&r;
-select rollno,name,total_mark into r,nm,total from studentmark1 where rollno =r;
-proc_grade(r,total,gr);
-dbms_output.put_line(r||nm||gr);
-insert into studentresult1 (rollno,name,grade)values(r,nm,gr);
-dbms_output.put_line('one record inserted');
-end;
+DECLARE
+gr VARCHAR2(20);
+BEGIN
+FOR rec IN (SELECT rollno, name, total_mark FROM studentmark1) 
+LOOP
+proc_grade(rec.rollno, rec.total_mark, gr);
+INSERT INTO studentresult1 (rollno, name, grade)
+VALUES (rec.rollno, rec.name, gr);
+DBMS_OUTPUT.PUT_LINE(rec.rollno || ' ' || rec.name || ' ' || gr);
+END LOOP;
+DBMS_OUTPUT.PUT_LINE('All records processed and inserted.');
+END;
 /
